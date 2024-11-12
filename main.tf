8bbea42b946b9dc25b20de0ef2b4755771232be5
bsantanad <benjamin.santana@proton.me>
2024-11-11 19:29:18 -0600

    main.tf: starting simple busybox server
    Signed-off-by: bsantanad <benjamin.santana@proton.me>

--- main.tf
+++ main.tf
@@ -7,7 +7,32 @@ resource "aws_instance" "example" {
     ami = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
 
+    # use terraform expressions to refer to the security rule we want to follow
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    # we would usually create an AMI for this with a real web app, using rails
+    # or smth, but this is just a simple example.
+    user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+    user_data_replace_on_change = true
+
     tags = {
         Name = "terraform-example"
     }
 }
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
